241d327224315ac6fc761314fa7608b864b72056
Liran <liran.napadenski@gmail.com>
2025-05-24 01:44:58 +0300

    finished

--- tabular_Q.py
+++ tabular_Q.py
@@ -11,6 +11,7 @@ Q = np.zeros([env.observation_space.n,env.action_space.n])
 lr = .8
 y = .95
 num_episodes = 2000
+NOISE = lambda x : 1/x
 #create lists to contain total rewards and steps per episode
 #jList = []
 rList = []
@@ -20,6 +21,7 @@ for i in range(num_episodes):
     rAll = 0 # Total reward during current episode
     d = False
     j = 0
+    FOUND_GOAL = False
     #The Q-Table learning algorithm
     while j < 99:
         j+=1
@@ -29,10 +31,25 @@ for i in range(num_episodes):
         # 3. Update Q-Table with new knowledge
         # 4. Update total reward
         # 5. Update episode if we reached the Goal State
+        action = 0
+        if np.random.rand() < NOISE(j) or not FOUND_GOAL:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(Q[s,:])
+        
+        ns, reward, terminated, _ = env.step(action)
+        Q[s, action] = Q[s, action] + lr * (reward + y * Q[ns,:].max() - Q[s, action])
+        
+        s = ns
+        rAll += reward
+        if reward != 0 :
+            FOUND_GOAL = True
+        if terminated:
+            break
     
     rList.append(rAll)
 
 # Reports
 print("Score over time: " +  str(sum(rList)/num_episodes))
 print("Final Q-Table Values")
-print Q
+print(Q)
